004721b5298d3edc0d0ef146c41ce38fa5acce8d
VladKachenya <38651074+VladKachenya@users.noreply.github.com>
2019-04-19 21:09:50 +0300

    +

---
+++ UnitTests/MazeLogicCoreTests/Builders/HeroBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿using MazeLogicCore.Builders;
+using MazeModelCore.Interfases.ComplexModels;
+using MazeModelCore.Interfases.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace MazeLogicCoreTests.Builders
+{
+    [TestFixture]
+    public class HeroBuilderTests
+    {
+        private Mock<IHero> _heroMock;
+        private Mock<IMaze> _mazeMock;
+        private Mock<IRoom> _roomMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mazeMock = new Mock<IMaze>();
+            _heroMock = new Mock<IHero>();
+            _roomMock = new Mock<IRoom>();
+            _mazeMock.Setup(a => a[0, 0]).Returns(() => _roomMock.Object);
+        }
+
+        [Test]
+        public void Build_SetHeroToMazeTest()
+        {
+            int getHeroCounter = 0;
+            bool isSerHeroToMazeCounter = false;
+            _roomMock.SetupSet(a => a.Content).
+                Callback((obj) => isSerHeroToMazeCounter = obj.Equals(_heroMock.Object));
+
+            new HeroBuilder(_heroMock.Object).Build(_mazeMock.Object);
+
+            Assert.IsTrue(isSerHeroToMazeCounter);
+        }
+
+    }
+}
